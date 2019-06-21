0a565a8c75c19325a4a7baddac5004bf8e381f39
尹大炮 <3235144364@qq.com>
2019-06-21 20:38:17 +0800

    用户登录-发送验证码-请求获取初始化的参数

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 import router from './router'
 
+// 引入公共样式文件，最好在 element 样式文件之后，可以自定义修改 element 内置样式
+import './styles/index.less'
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,88 @@
 <template>
-  <div>
-    <p>Login Component</p>
-  </div>
+   <div class="login-wrap">
+    <!-- 给组件加 class，会把这个 class 作用到组件的根元素上 -->
+    <div class="form-wrap">
+      <div class="form-head">
+        <img src="../logo_index.png" alt="黑马头条号">
+      </div>
+      <el-form class="form-content" ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <!-- el-col 栅格布局,一共24列，:span 用来指定占用的大小，:offset 用来制动偏移量 -->
+          <el-col :span="14">
+          <el-input v-model="form.code" placeholder="验证码"></el-input>
+          </el-col>
+          <el-col :offset="1" :span="9">
+            <el-button @click="handleSendCode">获取验证码</el-button>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'AppLogin',
   data() {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    },
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
+    }
   }
 }
+
 </script>
-<style lang="scss" scoped>
+
+ <style lang="less" scoped>
+  .login-wrap {
+      height: 100%;
+      background-color: #2b3e4a;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    .form-head {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+      img {
+         width: 200px;
+      }
+  }
+
+  .form-wrap {
+    width: 400px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
 </style>
