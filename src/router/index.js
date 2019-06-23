0be8f176e7b4fc2cf3d2831a725165de2883c3ff
尹大炮 <3235144364@qq.com>
2019-06-23 21:35:12 +0800

    layout布局组件-嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,31 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      name: 'home',
+    // {
+    //   name: 'home',
+    //   path: '/',
+    //   // 在整个目录项目中，模块路径中的 @ 表示的是src目录
+    //   // 无论你当前模块在哪里，@都可以直接定位到 src
+    //   // 加载一个目录可以默认加载它的 index.js、index.json、index.vue 。。。
+    //   // component: () => import('@/view/home/index.vue')
+    //   component: () => import('@/views/home')
+    // },
+    { // layout 显示到根数组的路由接口
+      // name: 'layout', // 使用 JaavaScript 命名导航不会渲染默认子路由
       path: '/',
-      // 在整个目录项目中，模块路径中的 @ 表示的是src目录
-      // 无论你当前模块在哪里，@都可以直接定位到 src
-      // 加载一个目录可以默认加载它的 index.js、index.json、index.vue 。。。
-      // component: () => import('@/view/home/index.vue')
-      component: () => import('@/views/home')
+      component: () => import('@/views/layout'),
+      children: [ // 所有 children 路由都显示到父路由的 router-view 中
+        {
+          name: 'home',
+          path: '/home',
+          component: () => import('@/views/home')
+        },
+        {
+          name: 'publish',
+          path: '/publish',
+          component: () => import('@/views/publish')
+        }
+      ]
     },
     {
       name: 'login',
---
+++ src/views/layout/components/AppAside.vue
@@ -0,0 +1,82 @@
+<template>
+   <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      :router="true"
+      >
+      <el-menu-item index="/">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+            <span>文章管理</span>
+          </template>
+        <el-menu-item index="/publish">
+          <i class="el-icon-menu"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="1-2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">内容列表</span>
+        </el-menu-item>
+        <el-menu-item index="1-3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="1-4">
+          <i class="el-icon-menu"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+            <span>粉丝管理</span>
+          </template>
+        <el-menu-item index="2-1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">图文数据</span>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">粉丝概况</span>
+        </el-menu-item>
+        <el-menu-item index="2-3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">粉丝列表</span>
+        </el-menu-item>
+      </el-submenu>
+       <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">账户管理</span>
+      </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'AppAside',
+  data () {
+    return {}
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
